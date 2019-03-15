2c70a16905f770016b5257b2f1f00333cd2e32aa
RomanMay <roman.maistrenko@nure.ua>
2019-03-15 14:35:24 +0200

    edit port to listen

--- .history/index_20190315143206.js
+++ .history/index_20190315143206.js
@@ -8,6 +8,6 @@ app.get('/', function (req, res) {
 	res.render('index');
 })
 
-app.listen(process.env.PORT || 5000, function () {
-	console.log('Example app listening on port 5000!')
+app.listen(3000, function () {
+	console.log('Example app listening on port 3000!')
 })
